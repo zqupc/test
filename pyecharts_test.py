8d7afa53949230a8891fcc584005cb2ca19503d7
anhuang <zq_upc@163.com>
2018-05-29 13:30:50 +0800

    提交

--- pyecharts_test.py
+++ pyecharts_test.py
@@ -1,6 +1,6 @@
 from pyecharts import Bar
 
 bar = Bar("我的第一个图表", "这里是副标题")
-bar.add("服装", ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"], [5, 20, 36, 10, 75, 90])
+bar.add("服装", ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"], [5, 20, 36, 10, 75, 90],is_more_utils=True)
 # bar.print_echarts_options() # 该行只为了打印配置项，方便调试时使用
-bar.render("e:\my_first_chart.html")
+bar.render(r"e:\my_first_chart.html")
